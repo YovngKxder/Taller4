3b98539f46877d4d1bf5edf3af93b9cdff12fea2
YovngKxder <maximiliano.abdelkader@alumnos.ucn.cl>
2026-07-03 23:33:54 -0400

    Update Main.java to skip blank lines

--- Desarrollo/Logica/Main.java
+++ Desarrollo/Logica/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
             while (sc.hasNextLine()) {
                 String linea = sc.nextLine();
-                if (linea != null) {
+                if (!linea.isBlank()) {
                     sistema.agregarCarta(linea);
                 }
             }
